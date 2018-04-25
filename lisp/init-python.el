27fb7fb343e98d59c3ad642b15c10ac8b02fc1d3
inter311 <inter311@126.com>
2018-04-25 16:23:27 +0800

    add elpy for python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,15 +4,19 @@
               auto-mode-alist))
 
 (require-package 'pip-requirements)
+(require-package 'elpy)
 
-(when (maybe-require-package 'anaconda-mode)
-  (after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-  (when (maybe-require-package 'company-anaconda)
-    (after-load 'company
-      (add-hook 'python-mode-hook
-                (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
+(elpy-enable)
+(setq python-shell-interpreter "ipython")
+
+;;;(when (maybe-require-package 'anaconda-mode)
+;;;  (after-load 'python
+;;;    (add-hook 'python-mode-hook 'anaconda-mode)
+;;;    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+;;;  (when (maybe-require-package 'company-anaconda)
+;;;    (after-load 'company
+;;;      (add-hook 'python-mode-hook
+;;;                (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
 
 
 (provide 'init-python)
